c2bd8a79782493aa872144af10c0e386d35118aa
AzureAD\HuynhMinhThang <hthang0308a@gmail.com>
2022-11-30 16:31:10 +0700

    hello message when access backend

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,4 +4,10 @@ import { JwtAuthGuard } from "./guards/jwt.guard";
 
 @Controller()
 export class AppController {
+	constructor(private readonly appService: AppService) { }
+
+	@Get()
+	getHello(): string {
+		return this.appService.getHello();
+	}
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from "./auth/auth.module";
 import { GroupModule } from "./group/group.module";
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 
 @Module({
 	imports: [
 		ConfigurationModule,
 		AuthModule,
-		GroupModule,
-		ServeStaticModule.forRoot({
-		rootPath: join(__dirname, '..', 'public'),
-		}),
+		GroupModule
 	],
 	controllers: [AppController],
 	providers: [AppService],
