b13bf93065825441951cd1ad3dc070c769d9b0c9
Nguyễn Thanh An <thanhan030301@gmail.com>
2022-11-30 13:34:43 +0700

    Add server static FE

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,7 @@
 import { Controller, Get, Post, UseGuards, Res } from '@nestjs/common';
-import { AuthGuard } from "@nestjs/passport";
 import { AppService } from './app.service';
 import { JwtAuthGuard } from "./guards/jwt.guard";
 
 @Controller()
 export class AppController {
-	constructor(
-		private readonly appService: AppService
-	) { }
-
-	@Get()
-	@UseGuards(JwtAuthGuard)
-	getHello(): string {
-		return this.appService.getHello();
-	}
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from "./auth/auth.module";
 import { GroupModule } from "./group/group.module";
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
 	imports: [
 		ConfigurationModule,
 		AuthModule,
 		GroupModule,
+		ServeStaticModule.forRoot({
+		rootPath: join(__dirname, '..', 'public'),
+		}),
 	],
 	controllers: [AppController],
 	providers: [AppService],
